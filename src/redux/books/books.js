3f99c2076d0223a0a125794b25345fec3189f28c
aakbarkhan <aakbarkhan@gmail.com>
2021-12-15 15:03:54 +0530

    Add the duck pattern and comments

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,5 +1,5 @@
-const ADD_BOOK = 'ADD_BOOK';
-const REMOVE_BOOK = 'REMOVE_BOOK';
+const ADD_BOOK = 'bookStore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 const initialState = [];
 
 export const addBook = (payload) => ({
